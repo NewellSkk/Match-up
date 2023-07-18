6d3e256abc0e41f15af71873bcc4fd7ac8941345
Newell <99129123+NewellSkk@users.noreply.github.com>
2023-07-18 13:25:23 +0000

    Update 2 files

--- src/App.js
+++ src/App.js
@@ -1,36 +1,72 @@
 import "./styles.css";
-
-function Square({type,value}) {
+import { useState } from "react";
+function Square({ type, value, match }) {
   return (
-    <div className="Square">
+    <div className="Square" onClick={() => match()}>
       <div className={type}>{value}</div>
     </div>
   );
 }
 
-function Screen({info}){
-  let bigBox=[];
-  info.forEach(element => {
-    if(bigBox.indexOf(element.country)===-1){
-      bigBox.push(
-        <Square type='Country' value={element.country} key={element.country}/>,
-        <Square type='Capital' value={element.capital} key={element.capital}/>,
-      )
+function Screen({ info }) {
+  let [Nat, setNat] = useState(" ");
+  let [Cap, setCap] = useState(" ");
+  let match = (value, type) => {
+    alert(`clicked value:${value}`);
+    if (type === "Capital") {
+      setCap(value);
+      alert(`Clicked cap:${Cap}`);
     }
-  
-  });
-  return(
-    <>
-      {bigBox}
-    </>
-  );  
+    if (type === "Country") {
+      setNat(value);
+      alert(`Clicked country:${Cap}`);
+    }
+    if (Cap === Nat) {
+      alert(`Match ${Nat}`);
+    } else {
+      alert(`Nat:${Nat} Cap:${Cap}`);
+    }
+  };
+  let bigBox = [];
 
+  info.forEach((element) => {
+    if (bigBox.indexOf(element.country) === -1) {
+      bigBox.push(
+        <Square
+          type="Country"
+          value={element.country}
+          key={element.country}
+          match={() => match(element.country, "Country")}
+        />,
+        <Square
+          type="Capital"
+          value={element.capital}
+          key={element.capital}
+          match={() => match(element.country, "Capital")}
+        />
+      );
+    }
+  });
+  return <>{bigBox}</>;
 }
+
+// const shuffleArray = (array) => {
+//   for (var i = array.length - 1; i > 0; i--) {
+//     var j = Math.floor(Math.random() * (i + 1));
+
+//     var temp = array[i];
+//     array[i] = array[j];
+//     array[j] = temp;
+//   }
+
+//   return array;
+// };
 let INFO = [
   { country: "Kenya", capital: "Nairobi" },
   { country: "Uganda", capital: "Kampala" },
   { country: "Egypt", capital: "Cairo" },
-  { country: "Ghana", capital: "Accra" }
+  { country: "Ghana", capital: "Accra" },
+  { country: "Nigeria", capital: "Lagos" }
 ];
 export default function App() {
   return (
