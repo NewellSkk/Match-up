f4ed35e3a87704d743e0b837e9e6cf5bc0485c0a
piusgori <piusgori@gmail.com>
2023-07-25 16:15:23 +0300

    Fix issue

--- src/App.js
+++ src/App.js
@@ -1,91 +1,57 @@
-import "./styles.css";
-import {useState} from 'react';
+import React, { useEffect, useState } from 'react';
+import './styles.css'
 
-function Square({value,match,clickFn}) {
-  return (
-    <div className="Square" 
-    onClick={
-      (e)=>{
-        clickFn(e,match)
-      }} >
-      {value}
-    </div>
-  );
-}
-function BigBox({info,type,select,setSelect}){
-
-  let entries=[];
-  let clickFn=(e,val)=>{
-    setSelect(prev=>{
-      if(type==="country"){
-        return{...prev,country:val}
-      }else{
-        return{...prev,capital:val}
-      } 
-    })
-    console.log(select)
-    alert(`${val} clicked`);
-     let preClicked=document.getElementsByClassName("Clicked")[0];
-     if(preClicked!=null){
-       preClicked.classList.remove("Clicked");
-     }
-     e.target.classList.add("Clicked");
-  }
-  info.forEach(entry => {
-    if (entries.indexOf(entry.country) === -1) {
-      entries.push(
-        <Square 
-          value={(type==="country")?entry.country:entry.capital}
-          key={entry.country}
-          match={entry.country}
-          clickFn={(e,value)=>clickFn(e,value) }
-        />
-      );
-    }
-  });
-  entries=shuffleArray(entries);
-  return(
-    <div className={type}>
-      <h1>{type}</h1>
-      <div>{entries}</div>
-    </div>
-  );
-}
-
-function Screen({ info }) {
-  let [select,setSelect]=useState({country:"",capital:""});
-  
-  return(
-    <div className="flex-container">
-    <BigBox info={info} type="country" select={select} setSelect={setSelect}/>
-    <BigBox info={info} type="capital"select={select} setSelect={setSelect}/>
-    </div>
-  );
-}
-
-
-const shuffleArray = (array) => {
-  for (var i = array.length - 1; i > 0; i--) {
-    var j = Math.floor(Math.random() * (i + 1));
-
-    var temp = array[i];
-    array[i] = array[j];
-    array[j] = temp;
-  }
-
-  return array;
-};
-let INFO = [
+const REGIONS = [
   { country: "Kenya", capital: "Nairobi" },
   { country: "Uganda", capital: "Kampala" },
   { country: "Egypt", capital: "Cairo" },
   { country: "Ghana", capital: "Accra" },
   { country: "Nigeria", capital: "Lagos" }
 ];
-export default function App() {
+
+const App = () => {
+
+  const [countries, setCountries] = useState(REGIONS.map(each => each.country));
+  const [capitals, setCapitals] = useState(REGIONS.map(each => each.capital));
+
+  const [selectedCountry, setSelectedCountry] = useState('');
+  const [selectedCapital, setSelectedCapital] = useState('');
+
+  useEffect(() => {
+    if (selectedCountry && selectedCapital) {
+      const foundRegion = REGIONS.find(each => each.country === selectedCountry);
+      if (foundRegion) {
+        if (selectedCapital === foundRegion.capital) {
+          setCountries(prev => prev.filter(each => each !== selectedCountry))
+          setCapitals(prev => prev.filter(each => each !== selectedCapital));
+          setSelectedCapital('');
+          setSelectedCountry('');
+        } else {
+          setSelectedCapital('');
+          setSelectedCountry('');
+        }
+      }
+    }
+  }, [selectedCountry, selectedCapital]);
+
   return (
-    <>
-      <Screen info={INFO} />
-    </>
-  );
+    <div className='overall'>
+      <ul>
+        {countries.map((each, index) => {
+          return (
+            <li className={selectedCountry === each && 'selected'} onClick={() => { setSelectedCountry(each) }} key={index}>{each}</li>
+          )
+        })}
+      </ul>
+      <ul>
+        {capitals.map((each, index) => {
+          return (
+            <li className={selectedCapital === each && 'selected'} onClick={() => { setSelectedCapital(each) }} key={index}>{each}</li>
+          )
+        })}
+      </ul>
+    </div>
+  )
 }
+
+export default App
